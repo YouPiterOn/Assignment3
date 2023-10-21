989cc759430f1fdc248e9d666f9e0a0ef2f1804b
youpiteron <oyushchenko@kse.org.ua>
2023-10-21 13:04:07 +0300

    commands added

--- Assignment3/Source.cpp
+++ Assignment3/Source.cpp
@@ -3,13 +3,25 @@
 using namespace std;
 
 int main() {
-	char text[10] = "something";
-	char* crypt = encrypt(text, 2);
-	cout << crypt << endl;
-	crypt = decrypt(crypt, 1);
-	cout << crypt << endl;
-	crypt = decrypt(crypt, 1);
-	cout << crypt;
-	cin >> text;
-	return 0;
+	char text[80];
+	int key = 0;
+	int command = 0;
+	while (true) {
+		cin >> command;
+		switch (command)
+		{
+		case 1:
+			scanf_s("%s", text, sizeof(text));
+			scanf_s("%i", &key);
+			cout << encrypt(text, key);
+			break;
+		case 2:
+			scanf_s("%s", text, sizeof(text));
+			scanf_s("%i", &key);
+			cout << decrypt(text, key);
+			break;
+		default:
+			break;
+		}
+	}
 }
